a2eaa4a8e078cd02c118a81dff34b63bb2c5089b
josh muleshi <jmuleshi2@gmail.com>
2020-12-31 08:52:57 +0000

    fin de mon api rest

--- takeContacts.php
+++ takeContacts.php
@@ -1,1 +1,15 @@
-<
+<?php
+
+require 'init.php';
+
+$sql = "SELECT * FROM contacts";
+
+$result = mysqli_query($conn, $sql);
+$response = array();
+while($row = mysqli_fetch_array($result)){
+    array_push($response,array('name'=>$row['name'],'email' =>$raw['email']));
+}
+
+echo json_encode($response);
+
+mysqli_close($conn);
